f5c1f81bd12e9c1bb3e221bbeeb89dff50178977
Cameron <seeingstars@startmail.com>
2026-07-05 22:14:09 -0600

    CircularProgressIndicator is now animated and flipText is properly updated and randomized

--- app/src/main/java/dev/spacebanana/coinflipper/presentation/MainActivity.kt
+++ app/src/main/java/dev/spacebanana/coinflipper/presentation/MainActivity.kt
@@ -8,18 +8,14 @@ package dev.spacebanana.coinflipper.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -28,46 +24,37 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.wear.compose.foundation.lazy.TransformingLazyColumn
 import androidx.wear.compose.foundation.lazy.rememberTransformingLazyColumnState
 import androidx.wear.compose.material3.AppScaffold
 import androidx.wear.compose.material3.Button
-import androidx.wear.compose.material3.ButtonColors
 import androidx.wear.compose.material3.ButtonDefaults
 import androidx.wear.compose.material3.CircularProgressIndicator
 import androidx.wear.compose.material3.CircularProgressIndicatorDefaults
-import androidx.wear.compose.material3.EdgeButton
-import androidx.wear.compose.material3.ListHeader
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.ProgressIndicatorDefaults
 import androidx.wear.compose.material3.ScreenScaffold
 import androidx.wear.compose.material3.ShapeDefaults
-import androidx.wear.compose.material3.Shapes
-import androidx.wear.compose.material3.SurfaceTransformation
 import androidx.wear.compose.material3.Text
-import androidx.wear.compose.material3.lazy.rememberTransformationSpec
-import androidx.wear.compose.material3.lazy.transformedHeight
 import androidx.wear.compose.ui.tooling.preview.WearPreviewDevices
 import androidx.wear.compose.ui.tooling.preview.WearPreviewFontScales
-import dev.spacebanana.coinflipper.R
 import dev.spacebanana.coinflipper.presentation.theme.CoinFlipperTheme
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.milliseconds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp("Android")
+            WearApp()
         }
     }
 }
 
 @Composable
-fun WearApp(greetingName: String) {
+fun WearApp() {
     val context = LocalContext.current
     var isFlipping: Boolean by remember { mutableStateOf(false) }
     val flippingTexts = remember {
@@ -81,10 +68,9 @@ fun WearApp(greetingName: String) {
     CoinFlipperTheme {
         AppScaffold {
             val listState = rememberTransformingLazyColumnState()
-            val transformationSpec = rememberTransformationSpec()
             ScreenScaffold(
                 scrollState = listState,
-            ) { contentPadding ->
+            ) {
                 Column(
                     modifier = Modifier
                         .fillMaxSize(),
@@ -95,7 +81,6 @@ fun WearApp(greetingName: String) {
                         Button(
                             modifier = Modifier.size(100.dp),
                             onClick = {
-                                flipText = flippingTexts.random()
                                 isFlipping = true
                             },
                             colors = ButtonDefaults.buttonColors(),
@@ -115,12 +100,21 @@ fun WearApp(greetingName: String) {
                             shape = ShapeDefaults.ExtraLarge,
                         )
                     } else {
-                        val currentProgress = 0f
-                        var progress by remember { mutableFloatStateOf(currentProgress) }
-                        val animatedProgress = animateFloatAsState(
-                            targetValue = progress,
-                            label = "flipProgress"
-                        ).value
+                        var progress by remember { mutableFloatStateOf(0f) }
+
+                        LaunchedEffect(isFlipping) {
+                            if (isFlipping) {
+                                for (i in 0..100) {
+                                    if (i % 20 == 0) {
+                                        flipText = flippingTexts.random()
+                                    }
+                                    progress = i / 100f
+                                    delay(30.milliseconds)
+                                }
+                                delay(500.milliseconds)
+                                isFlipping = false
+                            }
+                        }
 
                         Box(
                             modifier = Modifier.fillMaxSize(),
@@ -132,7 +126,7 @@ fun WearApp(greetingName: String) {
                                     indicatorColor = MaterialTheme.colorScheme.primary,
                                     trackColor = MaterialTheme.colorScheme.onBackground.copy(alpha = .3f),
                                 ),
-                                progress = { animatedProgress },
+                                progress = { progress },
                                 strokeWidth = CircularProgressIndicatorDefaults.largeStrokeWidth,
                             )
                             Text(
@@ -153,5 +147,5 @@ fun WearApp(greetingName: String) {
 @WearPreviewFontScales
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
